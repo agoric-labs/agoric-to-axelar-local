1db954a1cfdb2d8a97be2b83df3afd3b55da79d5
rabi-siddique <rabisiddique27@gmail.com>
2025-12-23 20:54:01 +0500

    chore: add _createAndDeposit implementation in Factory contract

--- packages/axelar-local-dev-cosmos/src/__tests__/contracts/Factory.sol
+++ packages/axelar-local-dev-cosmos/src/__tests__/contracts/Factory.sol
@@ -3,9 +3,35 @@ pragma solidity ^0.8.20;
 
 import {AxelarExecutable} from "@updated-axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
 import {IAxelarGasService} from "@updated-axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
-import {IERC20} from "@updated-axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol";
 import {StringToAddress, AddressToString} from "@updated-axelar-network/axelar-gmp-sdk-solidity/contracts/libs/AddressString.sol";
+
 import {Ownable} from "./Ownable.sol";
+
+// Minimal version taken from: https://github.com/Uniswap/permit2/blob/cc56ad0f3439c502c246fc5cfcc3db92bb8b7219/src/interfaces/IPermit2.sol
+interface IPermit2 {
+    struct TokenPermissions {
+        address token;
+        uint256 amount;
+    }
+
+    struct PermitTransferFrom {
+        TokenPermissions permitted;
+        uint256 nonce;
+        uint256 deadline;
+    }
+
+    struct SignatureTransferDetails {
+        address to;
+        uint256 requestedAmount;
+    }
+
+    function permitTransferFrom(
+        PermitTransferFrom calldata permit,
+        SignatureTransferDetails calldata transferDetails,
+        address owner,
+        bytes calldata signature
+    ) external;
+}
 
 struct CallResult {
     bool success;
@@ -31,10 +57,23 @@ struct CallMessage {
 error ContractCallFailed(string messageId, uint256 step);
 error InvalidSourceChain(string expected, string actual);
 
+// Payload that Factory receives from Axelar to create + deposit in a new Wallet
+struct CreateAndDepositPayload {
+    // the Agoric address string
+    string ownerStr;
+    // EVM address that signed the Permit2 EIP-712 (the token owner on this chain)
+    address tokenOwner;
+    // Permit2 SignatureTransfer permit
+    IPermit2.PermitTransferFrom permit;
+    // 65-byte or 64-byte (EIP-2098) signature
+    bytes signature;
+}
+
 contract Wallet is AxelarExecutable, Ownable {
     IAxelarGasService public gasService;
     string private constant EXPECTED_SOURCE_CHAIN = "agoric";
-    bytes32 private constant EXPECTED_SOURCE_CHAIN_HASH = keccak256(bytes(EXPECTED_SOURCE_CHAIN));
+    bytes32 private constant EXPECTED_SOURCE_CHAIN_HASH =
+        keccak256(bytes(EXPECTED_SOURCE_CHAIN));
 
     event CallStatus(
         string indexed id,
@@ -107,10 +146,15 @@ contract Factory is AxelarExecutable {
     using StringToAddress for string;
     using AddressToString for address;
 
-    address immutable _gateway;
+    address internal immutable _gateway;
     IAxelarGasService public immutable gasService;
+
+    // Permit2 SignatureTransfer entrypoint
+    IPermit2 public immutable permit2;
+
     string private constant EXPECTED_SOURCE_CHAIN = "agoric";
-    bytes32 private constant EXPECTED_SOURCE_CHAIN_HASH = keccak256(bytes(EXPECTED_SOURCE_CHAIN));
+    bytes32 private constant EXPECTED_SOURCE_CHAIN_HASH =
+        keccak256(bytes(EXPECTED_SOURCE_CHAIN));
 
     event SmartWalletCreated(
         address indexed wallet,
@@ -123,10 +167,12 @@ contract Factory is AxelarExecutable {
 
     constructor(
         address gateway_,
-        address gasReceiver_
+        address gasReceiver_,
+        address permit2_
     ) payable AxelarExecutable(gateway_) {
         gasService = IAxelarGasService(gasReceiver_);
         _gateway = gateway_;
+        permit2 = IPermit2(permit2_);
     }
 
     function _createSmartWallet(
@@ -142,6 +188,27 @@ contract Factory is AxelarExecutable {
         return newWallet;
     }
 
+    function _createAndDeposit(
+        string memory ownerStr,
+        address tokenOwner,
+        IPermit2.PermitTransferFrom memory permit,
+        bytes memory signature
+    ) internal returns (address newWallet) {
+        require(tokenOwner != address(0), "tokenOwner=0");
+        require(permit.permitted.token != address(0), "token=0");
+        require(permit.permitted.amount > 0, "amount=0");
+
+        newWallet = _createSmartWallet(ownerStr);
+
+        IPermit2.SignatureTransferDetails memory details = IPermit2
+            .SignatureTransferDetails({
+                to: newWallet,
+                requestedAmount: permit.permitted.amount
+            });
+
+        permit2.permitTransferFrom(permit, details, tokenOwner, signature);
+    }
+
     function _execute(
         bytes32 /*commandId*/,
         string calldata sourceChain,
@@ -151,7 +218,20 @@ contract Factory is AxelarExecutable {
         if (keccak256(bytes(sourceChain)) != EXPECTED_SOURCE_CHAIN_HASH) {
             revert InvalidSourceChain(EXPECTED_SOURCE_CHAIN, sourceChain);
         }
-        address smartWalletAddress = _createSmartWallet(sourceAddress);
+
+        // Decode creation + deposit data sent from Agoric
+        CreateAndDepositPayload memory p = abi.decode(
+            payload,
+            (CreateAndDepositPayload)
+        );
+
+        address smartWalletAddress = _createAndDeposit(
+            p.ownerStr,
+            p.tokenOwner,
+            p.permit,
+            p.signature
+        );
+
         emit SmartWalletCreated(
             smartWalletAddress,
             sourceAddress,
