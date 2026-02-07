c7775c5b70f2e40a2d9b7c94f23ef12a41285d49
Mathieu Hofman <mathieu@agoric.com>
2026-02-07 00:30:38 +0000

    More NatSpec updates

--- packages/axelar-local-dev-cosmos/src/contracts/ImmutableOwnable.sol
+++ packages/axelar-local-dev-cosmos/src/contracts/ImmutableOwnable.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
+/**
+ * @dev Provides a basic access control mechanism, to an immutable owner account
+ * that can be granted exclusive access to specific functions.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ *
+ * Like OpenZeppelin Ownable, but lacking a transfer mechanism.
+ * https://docs.openzeppelin.com/contracts/5.x/api/access#ownable
+ * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/fcbae5394ae8ad52d8e580a3477db99814b9d565/contracts/access/Ownable.sol
+ */
 abstract contract ImmutableOwnable {
     address public immutable owner;
 
